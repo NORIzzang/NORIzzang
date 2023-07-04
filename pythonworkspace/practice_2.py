fa0f66032d4fbd2fb494fa72c2bbd23a19d36b65
Norizzang <sksnfl@icloud.com>
2023-07-04 19:41:28 +0900

    python

--- pythonworkspace/practice_2.py
+++ pythonworkspace/practice_2.py
@@ -31,7 +31,7 @@ python = "Python is Amazing"
 print(python.lower()) #모든 문자를 소문자로 
 print(python.upper()) #모든 문자를 대문자로 
 print(python[0].isupper()) #대괄호 안에 있는 문자가 대문자 인지 구별 / True
-print(len(python)) #python 의 문자 길이 (띄어쓰기도 포함)
+print(len(python)) #python 의 문자 길이(갯수) (띄어쓰기도 포함)
 
 #특정 글자를 다른 글자로 변경하고 싶을 때
 print(python.replace("Python", "Java"))
@@ -109,4 +109,30 @@ print("Redd\bApple") #RedApple
 
 #\t 탭 
 print("Red\tApple") #Red  Apple
+
+"""Q) 사이트별로 비밀번호를 만들어주는 프로그램을 작성하시오 
+
+http://naver.com
+규칙1 : http:// 부분은 제외 => naver.com
+규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!" 로 구성
+
+예) 생성된 비밀번호 : nav51!"""
+
+# adress = "http://naver.com"
+# print(adress[7:11])
+# print(len(adress[7:11]))
+# print((adress[7:11]).count("e"))
+# print("!")
+
+#선생님 답변
+
+url = "http://naver.com"
+my_str = url.replace("http://","") #규칙1
+print(my_str)
+my_str = my_str[:my_str.index(".")] #규칙 2 , my_str[0:5] -> 0~5 직전까지 
+print(my_str)
+
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) +"!"
+print("{0} 의 비밀번호는 {1} 입니다." .format(url,password))
 
